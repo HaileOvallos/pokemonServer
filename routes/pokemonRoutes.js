33d2e195acfd4f97a404330b185084c4b20f2d78
HaileOvallos <ovalloshaile@gmail.com>
2024-12-18 21:22:10 -0500

    creacion catch pokemon

--- routes/pokemonRoutes.js
+++ routes/pokemonRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const {getPokemonStatus,createPokemonStatus,test} = require("../controllers/pokemonController")
+const {test,createPokemonStatus,getPokemonStatus,getPokemonByPokemonId,catchPokemonByPokemonId} = require("../controllers/pokemonController")
 const router = express.Router()
 
 router.get("/test",test)
 router.get("/",getPokemonStatus)
+router.get("/pokemonid/:pokemon_id",getPokemonByPokemonId)
+router.put("/catch/:pokemon_id",catchPokemonByPokemonId)
 router.post("/",createPokemonStatus)
 module.exports = router
